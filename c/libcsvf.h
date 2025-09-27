a1f35c9e074830b7761a20d94fb08daee151b00f
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2025-09-27 15:41:48 -0500

    Create libcsvf.h

---
+++ c/libcsvf.h
@@ -0,0 +1,7 @@
+#include <wchar.h>
+
+// get unicode version of csvf file
+wchar_t* csvf_char_gen(char* csvf);
+
+// Convert a csv file to a csvf file
+void csvToCsvf(char* inputName, char* outputName);
